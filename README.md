a98e66a9937fc601835951f9e7de10ed3c942f7c
Oleksandr Grytsov <al1img@gmail.com>
2017-07-19 16:53:40 +0300

    Add project structure
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # DisplayManager
+
+## Dependencies:
+### Required:
+* libxenbe log
+* libconfigc++
+* wayland-ivi-extension
+
+## How to configure
+```
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+cmake ${PATH_TO_SOURCES} -D${VAR1}=${VAL1} -D{VAR2}=${VAL2} ...
+```
+There are variables and options. Options can be set to ON and OFF.
+
+Supported options:
+
+| Option | Description |
+| --- | --- |
+| `WITH_DOC` | Creates target to build documentation. It required Doxygen to be installed. If configured, documentation can be create with `make doc` |
+
+Supported variables:
+
+| Variable | Description |
+| --- | --- |
+| `CMAKE_BUILD_TYPE` | `Release`, `Debug`, `RelWithDebInfo`, `MinSizeRel`|
+| `CMAKE_INSTALL_PREFIX` | Default install path |
+| `XENBE_LOG_INCLUDE_PATH` | Path to libxenbe includes if they are located in non standard place |
+
+Example:
+```
+// Debug build
+cmake ${PATH_TO_SOURCES} -DCMAKE_BUILD_TYPE=Debug
+```
+
+## How to build:
+```
+cd ${BUILD_DIR}
+make     // build sources
+make doc // build documentaion
+```
+## How to install
+```
+make install // to default location
+make DESTDIR=${PATH_TO_INSTALL} install //to other location
+```
+## How to run:
+```
+DisplayManager -c ${PATH_TO_CONFIG_FILE} -v${LOG_MASK}
+```
+
+Example:
+
+```
+DisplayManager -c display.cfg -v *:Debug
+```
---
+++ src/main.cpp
@@ -0,0 +1,166 @@
+/*
+ *  Backend
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * Copyright (C) 2016 EPAM Systems Inc.
+ */
+
+#include <fstream>
+#include <iostream>
+
+#include <csignal>
+#include <execinfo.h>
+#include <getopt.h>
+#include <unistd.h>
+
+#include <xen/be/Log.hpp>
+
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::string;
+
+using XenBackend::Log;
+
+string gCfgFileName;
+string gLogFileName;
+
+/*******************************************************************************
+ *
+ ******************************************************************************/
+
+void segmentationHandler(int sig)
+{
+	void *array[20];
+	size_t size;
+
+	LOG("Main", ERROR) << "Segmentation fault!";
+
+	size = backtrace(array, 20);
+
+	backtrace_symbols_fd(array, size, STDERR_FILENO);
+
+	exit(1);
+}
+
+void registerSignals()
+{
+	signal(SIGSEGV, segmentationHandler);
+}
+
+void waitSignals()
+{
+	sigset_t set;
+	int signal;
+
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	sigaddset(&set, SIGTERM);
+	sigprocmask(SIG_BLOCK, &set, nullptr);
+
+	sigwait(&set,&signal);
+}
+
+bool commandLineOptions(int argc, char *argv[])
+{
+
+	int opt = -1;
+
+	while((opt = getopt(argc, argv, "c:v:l:fh?")) != -1)
+	{
+		switch(opt)
+		{
+		case 'v':
+
+			if (!Log::setLogMask(string(optarg)))
+			{
+				return false;
+			}
+
+			break;
+
+		case 'c':
+
+			gCfgFileName = optarg;
+
+			break;
+
+		case 'l':
+
+			gLogFileName = optarg;
+
+			break;
+
+		case 'f':
+
+			Log::setShowFileAndLine(true);
+
+			break;
+
+		default:
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	try
+	{
+		registerSignals();
+
+		if (commandLineOptions(argc, argv))
+		{
+			ofstream logFile;
+
+			if (!gLogFileName.empty())
+			{
+				logFile.open(gLogFileName);
+				Log::setStreamBuffer(logFile.rdbuf());
+			}
+
+			LOG("Main", INFO) << "Start";
+
+			waitSignals();
+
+			LOG("Main", INFO) << "Stop";
+
+			logFile.close();
+		}
+		else
+		{
+			cout << "Usage: " << argv[0]
+				 << " [-c <file>] [-l <file>] [-v <level>]"
+				 << endl;
+			cout << "\t-c -- config file" << endl;
+			cout << "\t-l -- log file" << endl;
+			cout << "\t-v -- verbose level in format: "
+				 << "<module>:<level>;<module:<level>" << endl;
+			cout << "\t      use * for mask selection:"
+				 << " *:Debug,Mod*:Info" << endl;
+		}
+	}
+	catch(const std::exception& e)
+	{
+		Log::setStreamBuffer(cout.rdbuf());
+
+		LOG("Main", ERROR) << e.what();
+	}
+
+	return 0;
+}
