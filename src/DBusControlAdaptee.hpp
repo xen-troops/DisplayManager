4d46a25bd8fff4f752248bfb5dcb58bab3d7936a
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2019-05-30 17:43:45 +0300

    Used dbus-cxx lib instead of dbus-cpp
    dbus-cpp requires additional packages like process-cpp, properties-cpp.
    It is hard to maintain them with yocto.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/DBusControlAdaptee.hpp
@@ -0,0 +1,12 @@
+#ifndef DBUSCONTROLADAPTEE_HPP
+#define DBUSCONTROLADAPTEE_HPP
+
+#include <stdint.h>
+
+class DBusControlAdaptee
+{
+public:
+	virtual void userEvent(uint32_t event ) = 0;
+};
+
+#endif /* DBUSCONTROLADAPTEE_HPP */
---
+++ src/DBusControlAdapter.cpp
@@ -0,0 +1,27 @@
+#include "DBusControlAdapter.hpp"
+
+using DBus::Object;
+using DBus::MethodBase;
+
+using sigc::mem_fun;
+
+using std::shared_ptr;
+using std::string;
+
+DBusControlAdapter::DBusControlAdapter(
+	DBusControlAdaptee* adaptee, string path) : Object(path)
+{
+	MethodBase::pointer tmpMethod;
+	
+	tmpMethod = this->create_method<void,uint32_t>(
+		"com.epam.DisplayManager.Control", "userEvent",
+		mem_fun(adaptee, &DBusControlAdaptee::userEvent));
+
+	tmpMethod->set_arg_name(0, "event");
+}
+
+shared_ptr<DBusControlAdapter> DBusControlAdapter::create(
+	DBusControlAdaptee* adaptee, string path)
+{
+	return shared_ptr<DBusControlAdapter>(new DBusControlAdapter(adaptee, path));
+}
