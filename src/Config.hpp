5bbf9b699d52b777f16d4ad76bdc963d79a7587c
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2020-05-10 21:11:23 +0300

    Add proper file headers with license information
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Tested-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- src/Config.hpp
+++ src/Config.hpp
@@ -15,7 +15,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- * Copyright (C) 2016 EPAM Systems Inc.
+ * Copyright (C) 2017 EPAM Systems Inc.
  */
 
 #ifndef SRC_CONFIG_HPP_
